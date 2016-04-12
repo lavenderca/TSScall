a75174f09be993f24940c25708ad52ae60899ec5
Andy Lavender <c.andrew.lavender@gmail.com>
2016-04-12 16:33:49 -0400

    Added parameter 'set_read_threshold'

--- TSScall.py
+++ TSScall.py
@@ -102,6 +102,24 @@ class TSSCalling(object):
         self.annotation_join_distance = kwargs['annotation_join_distance']
         self.annotation_search_window = kwargs['annotation_search_window']
 
+        self.set_read_threshold = kwargs['set_read_threshold']
+        try:
+            int(self.set_read_threshold)
+        except:
+            pass
+        else:
+            self.set_read_threshold = int(self.set_read_threshold)
+
+        implied_threshold_methods = 0
+        for val in [self.fdr_threshold, self.false_positives, self.set_read_threshold]:
+            implied_threshold_methods += int(bool(val))
+        if implied_threshold_methods == 1:
+            pass
+        elif implied_threshold_methods > 1:
+            raise ValueError('More than 1 read threshold method implied!!')
+        elif implied_threshold_methods == 0:
+            self.false_positives = 1
+
         self.tss_list = []
         self.reference_annotation = None
         self.annotated_tss_count = 0
@@ -221,31 +239,31 @@ class TSSCalling(object):
                     loci += 1
             return loci
 
-        ## IF FDR OR EXPECTED FALSE POSITIVES IS NOT SET, DEFAULT TO 1 EXPECTED FALSE POSITIVE
-        if self.fdr_threshold is None and self.false_positives is None:
+        if self.fdr_threshold or self.false_positives:
             self.false_positives = 1
-        mappable_size = 0.8 * 2 * float(genome_size)
+            mappable_size = 0.8 * 2 * float(genome_size)
+            read_count = 0
+            for entry in bedgraph_list:
+                read_count += entry['reads']
+            expected_count = float(read_count)/mappable_size
 
-        read_count = 0
-        for entry in bedgraph_list:
-            read_count += entry['reads']
-        expected_count = float(read_count)/mappable_size
-
-        cume_probability = ((expected_count**0)/math.factorial(0))*math.exp(-expected_count)
-        threshold = 1
-        while True:
-            probability = 1 - cume_probability
-            expected_loci = probability * mappable_size
-            if self.fdr_threshold:
-                observed_loci = countLoci(bedgraph_list, threshold)
-                fdr = float(expected_loci)/float(observed_loci)
-                if fdr < fdr_threshold:
-                    return threshold
-            else:
-                if expected_loci < self.false_positives:
-                    return threshold
-            cume_probability += ((expected_count**threshold)/math.factorial(threshold))*math.exp(-expected_count)
-            threshold += 1
+            cume_probability = ((expected_count**0)/math.factorial(0))*math.exp(-expected_count)
+            threshold = 1
+            while True:
+                probability = 1 - cume_probability
+                expected_loci = probability * mappable_size
+                if self.fdr_threshold:
+                    observed_loci = countLoci(bedgraph_list, threshold)
+                    fdr = float(expected_loci)/float(observed_loci)
+                    if fdr < fdr_threshold:
+                        return threshold
+                else:
+                    if expected_loci < self.false_positives:
+                        return threshold
+                cume_probability += ((expected_count**threshold)/math.factorial(threshold))*math.exp(-expected_count)
+                threshold += 1
+        else:
+            return self.set_read_threshold
 
     ## FIND INTERSECTION WITH SEARCH_WINDOWS, BEDGRAPH_LIST;
     ## HITS ARE ADDED TO WINDOW_LIST, REQUIRES SORTED LIST
@@ -577,6 +595,7 @@ class TSSCalling(object):
         bedgraph_list = self.combineAndSortBedGraphs(self.forward_bedgraph, self.reverse_bedgraph)
         genome_size = self.findGenomeSize(self.chrom_sizes)
         read_threshold = self.findReadThreshold(bedgraph_list, genome_size)
+        print(read_threshold)
 
         if self.annotation_file:
             self.reference_annotation = readInReferenceAnnotation(self.annotation_file)
@@ -600,6 +619,7 @@ if __name__ == '__main__':
     parser.add_argument('--call_method', type=str, default='global', choices=['global', 'bin_winner'], help='TSS calling method to use (Default: global)')
     parser.add_argument('--annotation_join_distance', type=int, default=200, help='set INTEGER distace threshold for joining search windows from annotation')
     parser.add_argument('--annotation_search_window', type=int, default=1000, help='set annotation search window size to INTEGER')
+    parser.add_argument('--set_read_threshold', type=float, default=None, help='set read threshold for TSS calling to FLOAT; do not determine threshold from data')
     parser.add_argument('forward_bedgraph', type=str, help='forward strand Start-seq bedgraph file')
     parser.add_argument('reverse_bedgraph', type=str, help='reverse strand Start-seq bedgraph file')
     parser.add_argument('chrom_sizes', type=str, help='standard tab-delimited chromosome sizes file')
